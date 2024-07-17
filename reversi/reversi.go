ee534acc481a65a11842c33962d85b762b550e8b
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-07-17 23:40:08 +0900

    feat: 盤面を文字列で表示する機能を実装

--- reversi/reversi.go
+++ reversi/reversi.go
@@ -1,5 +1,10 @@
 package reversi
 
+import (
+	"strconv"
+	"strings"
+)
+
 type Stone int
 
 const (
@@ -12,6 +17,7 @@ type Reversi struct {
 	CurrStone Stone
 	Winner    Stone
 	Board     [8][8]Stone
+	b         strings.Builder
 }
 
 func NewReversi() Reversi {
@@ -22,4 +28,24 @@ func NewReversi() Reversi {
 			4: {3: Black, 4: White},
 		},
 	}
+}
+
+func (r *Reversi) String() string {
+	r.b.Reset()
+	r.b.WriteString("\n  a b c d e f g h\n")
+	for i, line := range r.Board {
+		r.b.WriteString(strconv.Itoa(i + 1))
+		for _, stone := range line {
+			switch stone {
+			case None:
+				r.b.WriteString(" ◌")
+			case Black:
+				r.b.WriteString(" ●")
+			case White:
+				r.b.WriteString(" ○")
+			}
+		}
+		r.b.WriteString("\n")
+	}
+	return r.b.String()
 }
